419f9ded0087d93c9a857132664074a6b7cb64d4
CODPUD <jalol.kham.2401@gmail.com>
2021-03-19 10:28:12 +0100

    Added resizable window setting

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .into_iter()
         .map(|(timestamp, price)| (NaiveDateTime::from_timestamp(timestamp as i64 / 1000, 0), price))
         .collect::<Vec<_>>();
-    let settings = iced::Settings::with_flags(gui::GuiFlags {
+    let mut settings = iced::Settings::with_flags(gui::GuiFlags {
         ids: cg_client.coins_list().await?.into_iter().map(|coin| coin.id).collect(),
         vs_currencies: cg_client.simple_supported_vs_currencies().await?,
         btc_to_usd
     });
+
+    settings.window = iced::window::Settings {
+        resizable: false,
+        ..Default::default()
+    };
+
     gui::Gui::run(settings)?;
     Ok(())
 }
