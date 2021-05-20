4c8d9388e2ee32c3db5bef96da7493d59d23eec6
Nazar Kalinowski <nazark@tutanota.com>
2021-05-20 23:53:04 +0300

    Implement smth with triggers

--- src/main.rs
+++ src/main.rs
@@ -51,39 +51,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
 }
 
-pub async fn check_triggers() -> Result<(), Box<dyn std::error::Error>> {
-    let api_client = coingecko_requests::api_client::Client::new();
-    let mut client = coingecko_requests::caching_client::Client::new(api_client).await?;
-    let triggers = client.get_all_triggers().await?;
+// pub async fn check_triggers() -> Result<(), Box<dyn std::error::Error>> {
+//     let api_client = coingecko_requests::api_client::Client::new();
+//     let mut client = coingecko_requests::caching_client::Client::new(api_client).await?;
+//     let triggers = client.get_all_triggers().await?;
 
-    for trigger in triggers {
-        let mut increase= true;
+//     for trigger in triggers {
+//         let mut increase= true;
 
-        if trigger.old_price > trigger.new_price {
-            increase = false;
-        }
+//         if trigger.old_price > trigger.new_price {
+//             increase = false;
+//         }
 
-        let coin_owned = vec![String::from(&trigger.coin)];
-        let coin_half_owned: Vec<_> = coin_owned.iter().map(String::as_str).collect();
+//         let coin_owned = vec![String::from(&trigger.coin)];
+//         let coin_half_owned: Vec<_> = coin_owned.iter().map(String::as_str).collect();
     
-        let currency_owned = vec![String::from(&trigger.currency)];
-        let currency_half_owned: Vec<_> = currency_owned.iter().map(String::as_str).collect();
+//         let currency_owned = vec![String::from(&trigger.currency)];
+//         let currency_half_owned: Vec<_> = currency_owned.iter().map(String::as_str).collect();
     
-        let price = client.price(&coin_half_owned, &currency_half_owned).await?;
-        let price = price[&trigger.coin][&trigger.currency];
+//         let price = client.price(&coin_half_owned, &currency_half_owned).await?;
+//         let price = price[&trigger.coin][&trigger.currency];
 
-        if (increase && price >= trigger.new_price) || (!increase && price <= trigger.new_price){
-            //println!("{} => {}\nOld Price: {}\nNew Price: {}\nChanged: {}", trigger.coin.to_uppercase(), trigger.currency.to_uppercase(), trigger.old_price, price, (price - trigger.old_price as f64).abs());
+//         if (increase && price >= trigger.new_price) || (!increase && price <= trigger.new_price){
+//             //println!("{} => {}\nOld Price: {}\nNew Price: {}\nChanged: {}", trigger.coin.to_uppercase(), trigger.currency.to_uppercase(), trigger.old_price, price, (price - trigger.old_price as f64).abs());
         
-            Notification::new()
-                .appname("JNA Monitor")
-                .summary(&format!("{} => {}", trigger.coin.to_uppercase(), trigger.currency.to_uppercase()))
-                .body(&format!("Old Price: {}\nCurrent Price: {}\nDifference: {}", trigger.old_price as i64, price, (price - trigger.old_price).abs() as i64))
-                .icon("D:/Projects/Organisation/mywork/JNA_Monitor/icon.png")
-                .show()?;
-        } 
+//             Notification::new()
+//                 .appname("JNA Monitor")
+//                 .summary(&format!("{} => {}", trigger.coin.to_uppercase(), trigger.currency.to_uppercase()))
+//                 .body(&format!("Old Price: {}\nCurrent Price: {}\nDifference: {}", trigger.old_price as i64, price, (price - trigger.old_price).abs() as i64))
+//                 .icon("D:/Projects/Organisation/mywork/JNA_Monitor/icon.png")
+//                 .show()?;
+//         } 
 
-    }
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
--- src/triggers_tab_gui.rs
+++ src/triggers_tab_gui.rs
@@ -1,5 +1,6 @@
 use std::{rc::Rc, sync::{Arc, RwLock}};
-use iced::{Clipboard, Column, Command, Length, PickList, Row, Text, pick_list, TextInput, text_input, Button, button, HorizontalAlignment};
+use coingecko_requests::data::{Coin, VsCurrency};
+use iced::{Button, Clipboard, Column, Command, HorizontalAlignment, Length, PickList, Row, Scrollable, Text, TextInput, button, pick_list, scrollable, text_input};
 
 pub struct Flags {
     pub coins: Rc<Vec<coingecko_requests::data::Coin>>,
@@ -9,11 +10,14 @@ pub struct Flags {
 
 #[derive(Debug, Clone)]
 pub enum Message {
+    TriggersUpdated(Vec<coingecko_requests::data::Trigger>),
     SaveTriggerClicked,
     CoinPicked(coingecko_requests::data::Coin),
     CurrencyPicked(coingecko_requests::data::VsCurrency),
     PriceInputChanged(String),
     TriggersAdded,
+    DeleteTriggerClicked(i64),
+    TriggerDeleted
 }
 
 #[derive(Debug, Clone)]
@@ -21,6 +25,7 @@ pub struct Gui {
     coins: Rc<Vec<coingecko_requests::data::Coin>>,
     currencies: Rc<Vec<coingecko_requests::data::VsCurrency>>,
     settings: Arc<RwLock<crate::settings::Settings>>,
+    triggers: Vec<coingecko_requests::data::Trigger>,
     picked_coin: coingecko_requests::data::Coin,
     coin_picklist_state: pick_list::State<coingecko_requests::data::Coin>,
     picked_currency: coingecko_requests::data::VsCurrency,
@@ -28,6 +33,8 @@ pub struct Gui {
     price_input_state: text_input::State,
     price_value: String,
     save_trigger_state: button::State,
+    scrollable_state: scrollable::State,
+    delete_button_states: Vec<button::State>,
 }
 
 impl Gui {
@@ -39,6 +46,7 @@ impl Gui {
             coins: flags.coins,
             currencies: flags.currencies,
             settings: flags.settings,
+            triggers: Vec::new(),
             coin_picklist_state: Default::default(),
             picked_coin: picked_coin.clone(),
             picked_currency: picked_currency.clone(),
@@ -46,11 +54,17 @@ impl Gui {
             price_input_state: Default::default(),
             price_value: Default::default(),
             save_trigger_state: Default::default(),
-        }, Command::none())
+            scrollable_state: Default::default(),
+            delete_button_states: Vec::new(),
+        }, Command::perform(update_triggers(), |result| Message::TriggersUpdated(result.unwrap())))
     }
 
     pub fn update(&mut self, message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
         match message {
+            Message::TriggersUpdated(vec) => {
+                println!("Triggers updated. len = {}", vec.len());
+                self.triggers = vec;
+            }
             Message::PriceInputChanged(value) => {
                 self.price_value = value;
             }
@@ -61,10 +75,19 @@ impl Gui {
                 self.picked_currency = picked;
             }
             Message::SaveTriggerClicked => {
-                let value = self.price_value.parse::<f64>().unwrap();
-                return Command::perform(add_triggers(self.picked_coin.raw.id.clone(), self.picked_currency.raw.name.clone(), value), |_x| Message::TriggersAdded);
+                if let Ok(value) = self.price_value.parse::<f64>() {
+                    return Command::perform(add_trigger(self.picked_coin.clone(), self.picked_currency.clone(), value), |x| { x.unwrap(); Message::TriggersAdded });
+                }
             }
-            Message::TriggersAdded => {}
+            Message::TriggersAdded => {
+                return Command::perform(update_triggers(), |result| Message::TriggersUpdated(result.unwrap()));
+            }
+            Message::DeleteTriggerClicked(id) => {
+                return Command::perform(delete_trigger(id), |result| { result.unwrap(); Message::TriggerDeleted });
+            }
+            Message::TriggerDeleted => {
+                return Command::perform(update_triggers(), |result| Message::TriggersUpdated(result.unwrap()));
+            }
         }
         Command::none()
     }
@@ -76,22 +99,36 @@ impl Gui {
         let coins = if show_all_coins { self.coins.as_ref().clone() } else { self.coins.iter().filter(|coin| coin.favourite).cloned().collect() };
         let currencies = if show_all_currencies { self.currencies.as_ref().clone() } else { self.currencies.iter().filter(|coin| coin.favourite).cloned().collect() };
 
-        //Text::new("Hello Triggers").into();
-        let mut main_column = Row::new().spacing(5);
-        let mut trigger_settings_row = Row::new().spacing(5).width(Length::Shrink);
+        self.delete_button_states = vec![Default::default(); self.triggers.len()];
+        let mut delete_button_states = self.delete_button_states.iter_mut().collect::<Vec<_>>();
 
+        let mut main_column = Column::new()
+            .spacing(5)
+            .width(Length::Fill)
+            .height(Length::Fill);
+        
+        let mut trigger_settings_row = Row::new()
+            .spacing(5)
+            .width(Length::Shrink)
+            .height(Length::Shrink);
 
-        let mut coin_column = Column::new().spacing(5).width(Length::FillPortion(1)); 
+        let mut coin_column = Column::new()
+            .spacing(5)
+            .width(Length::FillPortion(1)); 
         coin_column = coin_column.push(Text::new("Coin"));
         let coin_picklist = PickList::new(&mut self.coin_picklist_state, coins, Some(self.picked_coin.clone()), Message::CoinPicked).width(Length::Fill);
         coin_column = coin_column.push(coin_picklist);
 
-        let mut vs_currency_column = Column::new().spacing(5).width(Length::FillPortion(1));
+        let mut vs_currency_column = Column::new()
+            .spacing(5)
+            .width(Length::FillPortion(1));
         vs_currency_column = vs_currency_column.push(Text::new("Currency"));
         let vs_currency_picklist = PickList::new(&mut self.currency_picklist_state, currencies, Some(self.picked_currency.clone()), Message::CurrencyPicked).width(Length::Fill);
         vs_currency_column = vs_currency_column.push(vs_currency_picklist);
 
-        let mut price_input_column = Column::new().spacing(5).width(Length::FillPortion(1));
+        let mut price_input_column = Column::new()
+            .spacing(5)
+            .width(Length::FillPortion(1));
         price_input_column = price_input_column.push(Text::new("Enter a value"));
         let text_input_price = TextInput::new(&mut self.price_input_state,"200",&mut self.price_value ,Message::PriceInputChanged).width(Length::Fill).padding(5);
         price_input_column = price_input_column.push(text_input_price);
@@ -103,23 +140,51 @@ impl Gui {
 
         main_column = main_column.push(trigger_settings_row);
 
+        let mut scrollable = Scrollable::new(&mut self.scrollable_state)
+            .width(Length::Fill)
+            .height(Length::Fill);
+
+        for trigger in self.triggers.iter() {
+            let coin = self.coins.iter().find(|coin| coin.rowid == trigger.coin_id).cloned().unwrap();
+            let currency = self.currencies.iter().find(|currency| currency.rowid == trigger.currency_id).cloned().unwrap();
+            let initial_price = trigger.initial_price;
+            let target_price = trigger.target_price;
+            let mut trigger_row = Row::new().padding(5).spacing(5).width(Length::Fill);
+            trigger_row = trigger_row.push(Button::new(delete_button_states.pop().unwrap(), Text::new("delete")).on_press(Message::DeleteTriggerClicked(trigger.rowid)));
+            trigger_row = trigger_row.push(Text::new(format!("Trigger #{}: coin: {}, currency: {} from {} to {}", trigger.rowid, coin.raw.id, currency.raw.name, initial_price, target_price)));
+            scrollable = scrollable.push(trigger_row);
+        }
+
+        main_column = main_column.push(scrollable);
+
         main_column.into()
     }
 }
 
-pub async fn add_triggers(coin: String, currency: String, value: f64) -> Result<(), Box<dyn std::error::Error>>{
+pub async fn update_triggers() -> Result<Vec<coingecko_requests::data::Trigger>, Box<dyn std::error::Error>> {
     let api_client = coingecko_requests::api_client::Client::new();
-    let mut client = coingecko_requests::caching_client::Client::new(api_client).await?;
+    let client = coingecko_requests::caching_client::Client::new(api_client).await?;
 
-    let coin_owned = vec![String::from(&coin)];
-    let coin_half_owned: Vec<_> = coin_owned.iter().map(String::as_str).collect();
+    Ok(client.get_all_triggers().await?)
+}
 
-    let currency_owned = vec![String::from(&currency)];
-    let currency_half_owned: Vec<_> = currency_owned.iter().map(String::as_str).collect();
+pub async fn add_trigger(coin: Coin, currency: VsCurrency, value: f64) -> Result<(), Box<dyn std::error::Error>> {
+    let api_client = coingecko_requests::api_client::Client::new();
+    let client = coingecko_requests::caching_client::Client::new(api_client).await?;
 
-    let data = client.price(&coin_half_owned, &currency_half_owned).await?;
+    let data = client.price(&[coin.raw.id.as_str()], &[currency.raw.name.as_str()]).await?;
 
-    client.add_trigger(&coin, &currency, data[&coin][&currency], value).await?;
+    client.add_trigger(coin.rowid, currency.rowid, data[&coin.raw.id][&currency.raw.name], value).await?;
 
     Ok(())
+}
+
+pub async fn delete_trigger(trigger_id: i64) -> Result<(), Box<dyn std::error::Error>> {
+    let api_client = coingecko_requests::api_client::Client::new();
+    let client = coingecko_requests::caching_client::Client::new(api_client).await?;
+
+    client.delete_trigger(trigger_id).await?;
+
+    Ok(())
+
 }
